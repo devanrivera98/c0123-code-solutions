846e8beeddbed2523431a28865bf5456774ccedc
Devan Rivera <devanrivera98@gmail.com>
2023-01-31 18:07:32 +0000

    Completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,52 @@
+var orderHistory = [
+  {
+    itemName: 'Javascript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    cost: '$31.55',
+    totalCost: '$34.00',
+    orderPlaced: 'August 4, 2020',
+    orderNumber: 'Order#114-3941689-8772232',
+    delivered: 'August 8, 2020',
+    returnWindowClosed: 'September 7, 2020'
+  },
+  {
+    itemName: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    cost: '$41.33',
+    totalCost: '$44.53',
+    orderPlaced: 'July 19, 2020',
+    orderNumber: 'Order#113-9984268-1280257',
+    delivered: 'July 19, 2020',
+    returnWindowClosed: 'July 20, 2020'
+  },
+  {
+    itemName: 'Gamecube Controller Adapter: Super Smash Bros Switch Gamecube Adapter for Wii U, PC, Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    author: ' ',
+    cost: '$15.98',
+    totalCost: '$17.22',
+    orderPlaced: 'July 4, 2020',
+    orderNumber: 'Order#114-2875557-9059409',
+    delivered: 'July 7, 2020',
+    returnWindowClosed: ' August 5, 2020'
+  },
+  {
+    itemName: 'GameCube Controller - Super Smash Bros Edition (Nintentdo Switch)',
+    author: ' ',
+    cost: '$94.95',
+    totalCost: '$138.93',
+    orderPlaced: 'July 5, 2020',
+    orderNumber: 'Order#113-283177-2648248',
+    delivered: 'July 5, 2020',
+    returnWindowClosed: 'August 4, 2020'
+  },
+  {
+    itemName: 'The art of Sql',
+    author: ' ',
+    cost: '$33.99',
+    totalCost: '$138.93',
+    orderPlaced: 'July 5, 2020',
+    orderNumber: 'Order#113-283177-2648248',
+    delivered: 'July 5, 2020',
+    returnWindowClosed: 'August 4, 2020'
+  }
+];
