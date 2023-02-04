5b29361641ae913d4449c4197d394357157f6a31
Devan Rivera <devanrivera98@gmail.com>
2023-02-04 03:47:21 +0000

    Completed array manipulation 1 exercise.

---
+++ array-manipulation-1/src/head.js
@@ -0,0 +1,7 @@
+/* exported head */
+// define a function that uses the head function with an array as its arguement
+// return the first index of the array
+
+function head(array) {
+  return array[0];
+}
