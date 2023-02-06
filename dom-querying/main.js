f3e783de46f4f7c1fb2fa7826e4801f831694792
Devan Rivera <devanrivera98@gmail.com>
2023-02-06 18:00:11 +0000

    Completed dom-querying.

---
+++ dom-querying/main.js
@@ -0,0 +1,18 @@
+console.log('Hello, world');
+var el = document.querySelector('h1');
+console.log(el);
+console.dir(el);
+
+var ell = document.querySelector('#explanation');
+console.log(ell);
+console.dir(ell);
+
+var ella = document.querySelector('.hint');
+console.log(ella);
+console.dir(ella);
+
+var lee = document.querySelectorAll('p');
+console.log(lee);
+
+var le = document.querySelectorAll('.example-link');
+console.log(le);
