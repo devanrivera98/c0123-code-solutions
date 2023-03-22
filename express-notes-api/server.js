c22d88532bb603e179da1fd2bf193a709b44b140
Devan Rivera <devanrivera98@gmail.com>
2023-03-22 16:52:48 +0000

    part 1 and 2 of clients can POST a new note are complete

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -5,7 +5,8 @@ import { readFile } from 'node:fs/promises';
 const app = express();
 const data = readFileSync('data.json', 'utf-8');
 const dataParse = JSON.parse(data);
-const errorCode = { error: 'id must be a positive integer' };
+
+// GET STARTS BELOW
 
 readFile('data.json')
   .then((success) => {
@@ -27,12 +28,32 @@ readFile('data.json')
 app.get('/api/notes/:id', (req, res) => {
   const numberId = Number(req.params.id);
   if (numberId <= 0) {
+    const errorCode = { error: 'id must be a positive integer' };
     res.status(400).json(errorCode);
   } else if (dataParse.notes[numberId]) {
     res.status(200).json(dataParse.notes[numberId]);
   } else {
     const errorMessage = { error: `Cannot find note with id ${numberId}` };
     res.status(404).json(errorMessage);
+  }
+});
+
+// GET ENDS HERE
+
+app.use(express.json());
+
+// POST STARTS BELOW
+app.post('/api/notes', (req, res) => {
+  const newEntry = req.body;
+  // console.log(newEntry.content);
+  if (newEntry.content === undefined) {
+    const errorMessage = { error: 'Content is a required field' };
+    res.status(404).json(errorMessage);
+  } else {
+    newEntry.id = dataParse.nextId;
+    dataParse.nextId++;
+    console.log(newEntry);
+    res.status(201).json(newEntry);
   }
 });
 
