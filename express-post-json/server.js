8cdee4403f987f6008a9f76552df975258d2c915
Devan Rivera <devanrivera98@gmail.com>
2023-03-22 01:41:18 +0000

    Completed express-post-json

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -25,14 +25,3 @@ app.post('/api/grades', (req, res) => {
 app.listen(8080, () => {
   console.log('Listening on port 8080');
 });
-
-// app.post('/api/grades', (req, res) => {
-//   const eachGrade = req.body;
-//   eachGrade.id = nextId;
-//   nextId++;
-//   console.log(eachGrade);
-//   if (Object.keys(grades).length >= 0) {
-//     Object.assign(grades, eachGrade);
-//   }
-//   res.status(201).json(grades);
-// });
