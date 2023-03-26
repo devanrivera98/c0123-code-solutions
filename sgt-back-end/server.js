08c382e87fd393af4167c979a8ec9767ebc8f39d
Devan Rivera <devanrivera98@gmail.com>
2023-03-26 23:09:41 +0000

    Completed delete

--- sgt-back-end/server.js
+++ sgt-back-end/server.js
@@ -58,88 +58,53 @@ app.post('/api/grades', async (req, res) => {
     const result = await db.query(sql, params);
     const newGrade = result.rows[0];
     console.log(newGrade);
-    res.json(newGrade);
+    res.status(201).json(newGrade);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'An unexpected error occurred' });
   }
 });
 
-// app.post('/api/notes', (req, res) => {
-//   const newEntry = req.body;
-//   if (newEntry.content === undefined) {
-//     const errorMessage = { error: 'Content is a required field' };
-//     res.status(404).json(errorMessage);
-//   } else if (newEntry.content !== undefined) {
-//     newEntry.id = dataParse.nextId;
-//     dataParse.notes[dataParse.nextId] = newEntry;
-//     dataParse.nextId++;
-//     const dataStringify = JSON.stringify(dataParse, null, 2);
-//     writeFile('data.json', dataStringify)
-//       .then((success) => {
-//         res.status(201).json(newEntry);
-//         console.log('Content was saved');
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         res.status(500).json(error.message);
-//       });
-//   }
-// });
-// POST ENDS HERE
+// // DELETE STARTS HERE
+
+app.delete('/api/grades/:gradeId', async (req, res) => {
+  try {
+    const gradeId = Number(req.params.gradeId);
+    if (Number.isNaN(gradeId)) {
+      const errorMessage = { error: 'GradeId was not a number' };
+      res.status(400).json(errorMessage);
+      return;
+    } else if (gradeId < 0 || gradeId > 100) {
+      const errorMessage = { error: 'Score entered is above 100 or below 0' };
+      res.status(400).json(errorMessage);
+      return;
+    }
+    const sql = `
+    Delete
+      from "grades"
+      where "gradeId" = $1
+      Returning *
+    `;
+    const params = [gradeId];
+    const result = await db.query(sql, params);
+    const grade = result.rows[0];
+    console.log(grade);
+    if (grade) {
+      res.status(204).json(grade);
+    } else {
+      res.status(404).json({ error: `Cannot find grade with "gradeId"${gradeId}` });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: 'An unexpected error occurred' });
+  }
+});
+
+// // DELETE ENDS HERE
 
 app.listen(8080, () => {
   console.log('Listening on port 8080');
 });
-
-// // POST STARTS BELOW
-// app.post('/api/notes', (req, res) => {
-//   const newEntry = req.body;
-//   if (newEntry.content === undefined) {
-//     const errorMessage = { error: 'Content is a required field' };
-//     res.status(404).json(errorMessage);
-//   } else if (newEntry.content !== undefined) {
-//     newEntry.id = dataParse.nextId;
-//     dataParse.notes[dataParse.nextId] = newEntry;
-//     dataParse.nextId++;
-//     const dataStringify = JSON.stringify(dataParse, null, 2);
-//     writeFile('data.json', dataStringify)
-//       .then((success) => {
-//         res.status(201).json(newEntry);
-//         console.log('Content was saved');
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         res.status(500).json(error.message);
-//       });
-//   }
-// });
-// // POST ENDS HERE
-
-// // DELETE STARTS HERE
-
-// app.delete('/api/notes/:id', (req, res) => {
-//   const entryId = Number(req.params.id);
-//   if (entryId <= 0) {
-//     res.status(400).json(errorCodeNegative);
-//   } else if (entryId !== dataParse.notes[entryId].id) {
-//     const errorMessageNoId = { error: `Cannot find note with id ${entryId}` };
-//     res.status(404).json(errorMessageNoId);
-//   } else {
-//     delete dataParse.notes[entryId];
-//     const dataStringify = JSON.stringify(dataParse, null, 2);
-//     writeFile('data.json', dataStringify)
-//       .then((success) => {
-//         res.status(204).json(dataParse);
-//         console.log('Note was successfully deleted');
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         res.status(500).json(error.message);
-//       });
-//   }
-// });
-// // DELETE ENDS HERE
 
 // // PUT STARTS HERE
 // app.put('/api/notes/:id', (req, res) => {
