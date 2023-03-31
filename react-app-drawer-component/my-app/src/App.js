4cda40d79dae811c9c159a7a6dacbb951867b251
Devan Rivera <devanrivera98@gmail.com>
2023-03-31 17:15:12 +0000

    Fixed a line of code

--- react-app-drawer-component/my-app/src/App.js
+++ react-app-drawer-component/my-app/src/App.js
@@ -4,11 +4,10 @@ import './App.css';
 
 function App() {
   const menuList = [{ number: 1, name: 'About' }, { number: 2, name: 'Get Started' }, { number: 3, name: 'Sign In' }]
-  const menuHeading = 'Menu'
 
   return (
    <div>
-    <MyDrawer menuList={menuList} menuHeading={menuHeading}/>
+    <MyDrawer menuList={menuList} menuHeading="Menu"/>
    </div>
   );
 }
