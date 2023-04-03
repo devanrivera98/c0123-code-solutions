bff17493156d33c595e356c46cbdeab1d02bc2de
Devan Rivera <devanrivera98@gmail.com>
2023-04-03 22:13:57 +0000

    Moved server.js middleware and provide image

--- express-static/server.js
+++ express-static/server.js
@@ -1,8 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.use(express.static('public'));
 app.listen(8080, () => {
   console.log('Express server listening on port 8080');
 });
-
-app.use(express.static('public'));
