d9632ded3fd5a33e1b124cfc8527881862496fd1
Devan Rivera <devanrivera98@gmail.com>
2023-04-04 16:34:04 +0000

    Added another check response

--- fetch/main.js
+++ fetch/main.js
@@ -19,7 +19,10 @@ async function pokemonArray() {
   try {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/1', { method: 'GET' });
     const jsonData = await response.json();
-    console.log(jsonData);
+    if (response.ok) {
+      return console.log(jsonData);
+    }
+    throw new Error('Something went wrong');
   } catch (error) {
     console.error('There was an error', error);
   }
