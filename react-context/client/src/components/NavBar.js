2b274808c5c494066245f96f2328359fa9081477
Devan Rivera <devanrivera98@gmail.com>
2023-04-07 16:27:41 +0000

    Completed react-context

---
+++ react-context/client/src/components/NavBar.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-unused-vars -- Remove me */
+import React, { useContext } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import { FaBolt, FaSignOutAlt } from 'react-icons/fa';
+import AppContext from '../components/AppContext';
+
+export default function Navbar() {
+  /* TODO: Grab `user` and `handleSignOut` from `AppContext` */
+  // const user = undefined;
+  // const handleSignOut = undefined;
+  let {user, handleSignIn, handleSignOut} = useContext(AppContext)
+
+  console.log('User in NavBar', user)
+  console.log('handlesignout in NavBar', handleSignOut)
+
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            <FaBolt className="mr-2" />
+            Awesome App
+          </Link>
+          <div>
+            {user &&
+              <button className="btn btn-dark" onClick={handleSignOut}>
+                Sign out
+                <FaSignOutAlt className="ml-2" />
+              </button>
+            }
+            {!user &&
+              <>
+                <Link to="/sign-in" className="btn btn-primary">
+                  Sign In
+                </Link>
+                <Link to="/sign-up" className="btn btn-dark">
+                  Sign Up
+                </Link>
+              </>
+            }
+          </div>
+        </div>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
